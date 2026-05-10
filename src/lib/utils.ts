f1325ae8cfdfca45993963d5490f478a78b49779
son <s0nprem0@proton.me>
2026-05-10 21:04:15 +0800

    feat: install tailwind-merge and clsx, init utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+import {clsx, type ClassValue} from 'clsx';
+import {twMerge} from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('en-PH', {
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+  }).format(date);
+}
+
+export function truncateText(text: string, maxLength: number) {
+	if (text.length <= maxLength) return text;
+	return text.slice(0, maxLength) + '...';
+};

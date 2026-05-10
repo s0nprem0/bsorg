0b6a14b8d4c2e87fbd6b02a5efa0e58ac02bdf78
son <s0nprem0@proton.me>
2026-05-10 20:52:12 +0800

    chore: cleanup and refactor alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AcademicOrg from './pages/AcademicOrg';
-import NonAcademicOrg from './pages/NonAcademicOrg';
-import College from './pages/College';
-import Pag from './pages/Pag';
-import OrganizationProfile from './pages/OrganizationProfile';
-import NavBar from './components/layout/NavBar';
+import Home from '@/pages/Home';
+import AcademicOrg from '@/pages/AcademicOrg';
+import NonAcademicOrg from '@/pages/NonAcademicOrg';
+import College from '@/pages/College';
+import Pag from '@/pages/Pag';
+import OrganizationProfile from '@/pages/OrganizationProfile';
+import NavBar from '@/components/layout/Navbar';
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/acadorg" element={<AcademicOrg />} />
-        <Route path="/non-acadorg" element={<NonAcademicOrg />} />
-        <Route path="/college" element={<College />} />
-        <Route path="/pag" element={<Pag />} />
-        <Route path="/organization/:slug" element={<OrganizationProfile />} />
-      </Routes>
+      <div className='min-h-screen flex flex-col'>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/acadorg" element={<AcademicOrg />} />
+          <Route path="/non-acadorg" element={<NonAcademicOrg />} />
+          <Route path="/college" element={<College />} />
+          <Route path="/pag" element={<Pag />} />
+          <Route path="/organization/:slug" element={<OrganizationProfile />} />
+        </Routes>
+      </div>
     </Router>
   );
 }

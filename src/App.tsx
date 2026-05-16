4d8ddd5a36926580c8ce6a647a78532d3bf9967b
son <s0nprem0@proton.me>
2026-05-16 14:05:45 +0800

    feat: remove pag page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Home from '@/pages/Home';
 import AcademicOrg from '@/pages/AcademicOrg';
 import NonAcademicOrg from '@/pages/NonAcademicOrg';
 import College from '@/pages/College';
-import Pag from '@/pages/Pag';
 import OrganizationProfile from '@/pages/OrganizationProfile';
 import OrgBrowser from '@/pages/OrgBrowser';
 import NavBar from '@/components/layout/Navbar';
@@ -18,7 +17,6 @@ function App() {
           <Route path="/organization/acad-org" element={<AcademicOrg />} />
           <Route path="/organization/non-acadorg" element={<NonAcademicOrg />} />
           <Route path="/college" element={<College />} />
-          <Route path="/pag" element={<Pag />} />
           <Route path="/organization" element={<OrgBrowser />} />
           <Route path="/organization/:slug" element={<OrganizationProfile />} />
         </Routes>
--- src/pages/Pag.tsx
+++
@@ -1,8 +0,0 @@
-export default function Pag() {
-  return (
-    <div>
-      <h2>Performing Arts Group</h2>
-      <p>Information about Performing Arts Groups.</p>
-    </div>
-  );
-}
